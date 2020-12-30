11a45eb891a5bee4896c2ed24657988a0f58b8a9
RifkiMaulana123 <maulanfikri98@gmail.com>
2020-12-30 16:29:19 +0700

    menambahkan fungsi kategori

---
+++ app/src/main/java/com/example/quizin_kelompok_a/Kategori.java
@@ -0,0 +1,14 @@
+package com.example.quizin_kelompok_a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kategori extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kategori);
+    }
+}
