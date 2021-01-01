243460860f3e88fc586ba45556bfcf335f59f2de
fashap <patriafasha@gmail.com>
2021-01-01 16:35:39 +0700

    Menyelesaikan fungsi register dan login

--- app/src/main/java/com/example/quizin_kelompok_a/Register.java
+++ app/src/main/java/com/example/quizin_kelompok_a/Register.java
@@ -2,15 +2,14 @@ package com.example.quizin_kelompok_a;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,52 +18,50 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class Login extends AppCompatActivity {
-
-    private Button btnLogin;
-    private Button btnRegis;
+public class Register extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private Button btnRegister;
     private EditText etEmail, etPassword;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-
+        setContentView(R.layout.register);
+        btnRegister = findViewById(R.id.btn_register);
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
         mAuth = FirebaseAuth.getInstance();
 
-        btnLogin = findViewById(R.id.btn_login);
-        btnRegis = findViewById(R.id.btn_register);
-        etEmail = findViewById(R.id.et_username);
-        etPassword = findViewById(R.id.et_password);
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String email = etEmail.getText().toString();
                 String password = etPassword.getText().toString();
 
-                if (email.equals("")){
-                    Toast.makeText(Login.this, "Silahkan masukkan email anda", Toast.LENGTH_SHORT).show();
-                }else if (password.equals("")){
-                    Toast.makeText(Login.this, "Silahkan masukkan password anda", Toast.LENGTH_SHORT).show();
-                }else{
-                    mAuth.signInWithEmailAndPassword(email, password)
-                            .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                if (email.equals("")) {
+                    Toast.makeText(Register.this, "Isi email anda",
+                            Toast.LENGTH_SHORT).show();
+                } else if (password.equals("")) {
+                    Toast.makeText(Register.this, "Isi password anda",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
                                         // Sign in success, update UI with the signed-in user's information
                                         FirebaseUser user = mAuth.getCurrentUser();
-                                        Toast.makeText(Login.this, "Berhasil Login.",
+                                        Toast.makeText(Register.this, "Register berhasil.",
                                                 Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(getApplicationContext(), Home.class);
+                                        Intent intent = new Intent(getApplicationContext(), Login.class);
                                         startActivity(intent);
                                     } else {
                                         // If sign in fails, display a message to the user.
-                                        Toast.makeText(Login.this, "Login Gagal.",
+                                        Toast.makeText(Register.this, "Gagal register.",
                                                 Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(getApplicationContext(), Login.class);
+                                        Intent intent = new Intent(getApplicationContext(), Register.class);
                                         startActivity(intent);
                                     }
 
@@ -72,14 +69,6 @@ public class Login extends AppCompatActivity {
                                 }
                             });
                 }
-            }
-        });
-
-        btnRegis.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Register.class);
-                startActivity(intent);
             }
         });
     }
